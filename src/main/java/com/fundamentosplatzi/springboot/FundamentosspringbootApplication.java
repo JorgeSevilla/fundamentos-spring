ddffefea455f73d87cedbc0081b30adff7d07d3d
Jorge Sevilla <jorge.garcia@compasso.com.br>
2022-11-16 23:52:35 -0300

    Estructura del proyecto

---
+++ src/main/java/com/fundamentosplatzi/springboot/FundamentosspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosspringbootApplication.class, args);
+	}
+
+}
